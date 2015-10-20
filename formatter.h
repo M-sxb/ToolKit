76f5de09231f1a19375a0a61229eadf92a6e14ef
M <821312531@qq.com>
2015-10-20 19:24:57 +0800

    10-20-2015, modify ParamType

--- formatter.h
+++ formatter.h
@@ -16,11 +16,18 @@
 #ifndef Formatter_Head
 #define Formatter_Head
 
+namespace type { 
+
+namespace details {
+	struct true_ {};
+	struct false_ {};
+} // namespace details
+
 // is plain old data
 template<typename T>
-struct IsPOD { enum {value = false};};
+struct IsPOD { enum {value = false}; typedef details::false_ type;};
 
-#define IS_POD(type) 			template<> struct IsPOD<type> { enum {value = true};}; 
+#define IS_POD(t) 				template<> struct IsPOD<t> { enum {value = true}; typedef details::true_ type;}; 
 #define IS_POD_E(extra,type) 	IS_POD(type) \
  								IS_POD(extra type)
 // char
@@ -51,16 +58,32 @@ struct ParamTypeImp<false, T> { typedef T const& type;};
 template<typename T>
 struct ParamTypeImp<false, T&> { typedef T const& type;};
 */
-template<bool, typename T>
-struct ParamTypeImp { typedef T type;};
-template<typename T>
-struct ParamTypeImp<false, T> { typedef T const& type;};
-template<typename T>
-struct ParamTypeImp<false, T&> { typedef T const& type;};
+
+
+template<typename Bool, typename Type>
+struct ParamTypeImp { 
+	template<typename B, typename T>
+	struct apply {
+		typedef T type;
+	};
+
+	template<typename T>
+	struct apply<details::false_, T> {
+		typedef T const& type;
+	};
+
+	template<typename T>
+	struct apply<details::false_, T&> {
+		typedef T const& type;
+	};
+
+	typedef typename apply<typename Bool::type, Type>::type type;
+};
 
 template<typename T>
-struct ParamType : public ParamTypeImp<IsPOD<T>::value, T> { 
-};
+struct ParamType : public ParamTypeImp<IsPOD<T>, T> {};
+
+} // namespace type
 
 /* 
 // vc 6.0 ok
@@ -76,6 +99,9 @@ template<typename T>
 struct ParamTypeImp<false, T> { typedef T const& type;};
 template<typename T>
 struct ParamTypeImp<false, T&> { typedef T const& type;};
+
+template<typename T>
+struct ParamType : public ParamTypeImp<IsPOD<T>::value, T> {};
 */
 
 template<typename Info, typename Out>
@@ -83,7 +109,28 @@ struct Formatter {
 	typedef typename ParamType<Info>::type InfoParam;
 	typedef typename ParamType<Out>::type OutParam;
 	virtual ~Formatter() {};
-	virtual bool format(InfoParam info, OutParam out);
+	virtual bool format(InfoParam info, OutParam out) = 0;
+};
+
+template<typename Info, typename Out>
+class AggregateFormatter : public Formatter<Info, Out> { 
+	typedef typename std::vector<std::share_ptr<Formatter<Info, Out> > > Container;
+	typedef typename Container::iterater	iterater;
+
+	void addFormatter(const std::share_ptr<Formatter<Info, Out> >& ptr) { 
+		vformater_.push_back(ptr);s
+	}
+	virtual bool format(InfoParam info, OutParam out) { 
+		bool result = false;
+		iterater iter = vformater_.begin();
+		while(iter != vformater_.end()) { 
+			result = *iter->formater(info, out);
+			++iter;
+		}
+		return result;
+	}
+public:
+	std::vector<std::share_ptr<Formatter<Info, Out> > > vformater_;
 };
 
 
